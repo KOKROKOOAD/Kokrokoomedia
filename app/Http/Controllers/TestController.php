5aad8d60a51f12f44475428fc48bc6d61d3246cb
Joojo Arthur <jarthur@nalosolutions.com>
2020-01-18 07:06:08 +0000

    test uploads

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -44,7 +44,7 @@ class TestController extends Controller
 //     }
 
   public function  imagePaths(){
-      $path  = Storage::disk('docs');
+      $path  = Storage::disk('docs')->get('1579263995_Francis_Ocrannalo.jpeg');
       dd($path);
   }
 }
