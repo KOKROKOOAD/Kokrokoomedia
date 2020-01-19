c67894c966718572e73b9cd3c45f5351cb845c06
Joojo Arthur <jarthur@nalosolutions.com>
2020-01-19 13:56:35 +0000

    fix subscriptions datatable errors

--- app/Http/Controllers/RateCardController.php
+++ app/Http/Controllers/RateCardController.php
@@ -34,8 +34,8 @@ class RateCardController extends Controller
                     return $btn;
 
                     $btn = '<div class="btn-group btn-group-sm"> ';
-                    $btn =$btn.  '<button data-toggle="tooltip"  data-id="'.$row->rate_card_id.'" data-original-title="Edit" class="edit btn btn-success btn-sm accept-user" id="accept-users"><i class="fa fa-check"></i></button>';
-                    $btn = $btn.' <button data-toggle="tooltip"  data-id="'.$row->rate_card_id.'" data-original-title="Delete" class="btn btn-danger btn-sm block-user"><i class="fa fa-trash"></i> </button>';
+                    $btn =$btn.  '<button data-toggle="tooltip"  data-id="'.$row->rate_card_id.'" data-original-title="Edit" class="edit btn btn-success btn-sm ><i class="fa fa-check"></i></button>';
+                    $btn = $btn.' <button data-toggle="tooltip"  data-id="'.$row->rate_card_id.'" data-original-title="Delete" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> </button>';
                     $btn = $btn . '</div';
                     return $btn;
                 })
