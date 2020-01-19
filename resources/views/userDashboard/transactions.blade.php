a3773c67bf22d88c9e46042743a7cded96ee786c
Joojo Arthur <jarthur@nalosolutions.com>
2020-01-19 11:37:45 +0000

    download sub files

--- resources/views/userDashboard/transactions.blade.php
+++ resources/views/userDashboard/transactions.blade.php
@@ -28,7 +28,7 @@
 {{--            <search></search>--}}
 
             <div  class="dt-responsive table-responsive">
-                    <table id="laravel_datatable" class="table table-striped table-bordered nowrap">
+                    <table id="dom-table" class="table table-striped table-bordered nowrap">
                         <thead>
                         <tr>
                             <th>ID</th>
