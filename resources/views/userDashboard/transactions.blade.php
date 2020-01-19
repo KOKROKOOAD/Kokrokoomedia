59de89b0cab2a885f8de3cdab876e7c25665a7c1
Joojo Arthur <jarthur@nalosolutions.com>
2020-01-19 11:31:13 +0000

    download sub files

--- resources/views/userDashboard/transactions.blade.php
+++ resources/views/userDashboard/transactions.blade.php
@@ -25,33 +25,8 @@
         <div class="card">
 {{--            <search></search>--}}
 
-            <div class="card-block table-border-style">
-                <div class="table-responsive">
-{{--                    <table   class="table  table-striped table-bordered nowrap">--}}
-{{--                        <thead>--}}
-{{--                        <tr class="table-primary">--}}
-{{--                            <th>#</th>--}}
-{{--                            <th>Transaction Date</th>--}}
-{{--                            <th>Transaction id</th>--}}
-{{--                            <th>Payment source</th>--}}
-{{--                            <th>Amount Paid(GHC)</th>--}}
-{{--                            <th>Status</th>--}}
-{{--                        </tr>--}}
-{{--                        </thead>--}}
-{{--                        <tbody>--}}
-{{--                        <tr v-for="(trans,index) in transactions">--}}
-{{--                            <th scope="row">{{index + 1}}.</th>--}}
-{{--                            <td>{{trans.created_at}}</td>--}}
-{{--                            <td>{{trans.transaction_id}}</td>--}}
-{{--                            <td>{{trans.service}}</td>--}}
-{{--                            <td>{{trans.payment_source}}</td>--}}
-{{--                            <td>{{trans.amount}}</td>--}}
-{{--                            <!--                                <td></td>-->--}}
-
-{{--                        </tr>--}}
-{{--                        </tbody>--}}
-{{--                    </table>--}}
-                    <table id="laravel_datatable" class="table table-striped table-bordered table-responsive">
+            <div  class="dt-responsive table-responsive">
+                    <table id="laravel_datatable" class="table table-striped table-bordered nowrap">
                         <thead>
                         <tr>
                             <th>ID</th>
@@ -72,7 +47,6 @@
             </div>
         </div>
     </div>
-</div>
 
 @endsection
 
