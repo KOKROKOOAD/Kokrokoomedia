f7f8dfe4e8cfddcadc8c23635235248ea1562e68
Joojo Arthur <jarthur@nalosolutions.com>
2020-01-19 12:57:31 +0000

    fix subscriptions datatable errors

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -561,7 +561,7 @@
                     <script src="../files/bower_components/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js" type="07d460b7aca8f7ed44a5121c-text/javascript"></script>
 {{--                    <script src="../files/assets/pages/data-table/js/data-table-custom.js" type="07d460b7aca8f7ed44a5121c-text/javascript"></script>--}}
 
-                    <script src="{{ asset('js/datatable-files/transactions.js') }}"></script>
+{{--                    <script src="{{ asset('js/datatable-files/transactions.js') }}"></script>--}}
                     <script src="{{ asset('js/datatable-files/subscriptions.js') }}"></script>
 
 
