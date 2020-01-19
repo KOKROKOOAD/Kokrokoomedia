26c942230fbf4b07f5c76c9462f73a3ce7159e6e
Joojo Arthur <jarthur@nalosolutions.com>
2020-01-19 14:13:56 +0000

    fix subscriptions datatable errors

---
+++ public/js/datatable-files/downloadable.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    let table = $('#laravel_datatable');
+    table.DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: "ratecards",
+            type: 'GET',
+        },
+        columns: [
+            {data: 'id', name: 'id', 'visible': false},
+            {data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false},
+            {data: 'created_at', name: 'created_at'},
+            {data: 'segment', name: 'segment'},
+            {data: 'created_at', name: 'created_at'},
+            {data: 'updated_at',adjust:true, name: 'updated_at'},
+            {data: 'action', name: 'action', orderable: false},
+        ],
+        order: [[0, 'desc']],
+        "pageLength": 10,
+
+
+    });
+
+
+//hide datatable search box and label
+//     $('input[type=search]').hide();
+//     $('#laravel_datatable_filter label').hide();
+//
+//     // key up event for custom search
+//     oTable = table.DataTable();   //pay attention to capital D, which is mandatory to retrieve "api" datatables' object, as @Lionel said
+//     $('#search').keyup(function () {
+//         oTable.search($(this).val()).draw();
+//     });
+
+    //  $('#media_id').hide();
+
+});
