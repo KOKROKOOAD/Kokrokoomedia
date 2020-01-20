eca93f0eb353730e6ddefdf3970171f69f16a109
Joojo Arthur <jarthur@nalosolutions.com>
2020-01-20 03:08:32 +0000

    fix  view and approve sub logic

--- routes/web.php
+++ routes/web.php
@@ -285,185 +285,185 @@ Route::post('check-login', 'DecideControllers\InterceptAuthController@whoIsLogin
 
 // MEDIA STAFF ROUTES
 
-Route::prefix('media/staff')->group(function () {
-    Route::get('dashboard', function () {
-        return view('staffDashboard.dashboard');
-    })->name('staff.dashboard');
-
-    Route::get('active-subscription', function () {
-        return view('staffDashboard.activeSubscription');
-    })->name('staff.sub.active');
-
-
-    Route::get('inactive-subscription', function () {
-        return view('staffDashboard.inactiveSubscription');
-    })->name('staff.sub.inactive');
-
-    Route::get('expired-subscription', function () {
-        return view('staffDashboard.expiredSubscription');
-    })->name('staff.sub.expired');
-
-
-    Route::get('pending-subscription', function () {
-        return view('staffDashboard.pendingSubscription');
-    })->name('staff.sub.pending');
-
-    Route::get('transactions', function () {
-        return view('staffDashboard.transactions');
-    })->name('staff.transact');
-
-    Route::get('user-profile', function () {
-        return view('staffDashboard.accountSettings');
-    })->name('staff.profile');
-
-    Route::get('new-subscriptions', function () {
-        return view('staffDashboard.newSubscription');
-    })->name('staff.sub.new');
-
-    Route::get('dashboard', function () {
-        $media = \App\User::all('logo');
-        return view('staffDashboard.dashboard', ['media' => $media]);
-    })->name('staff.dashboard');
-
-    Route::get('subscription', function () {
-        return view('staffDashboard.createSub');
-    })->name('staff.sub.all');
-
-    Route::get('rejected-subscription', function () {
-        return view('staffDashboard.rejectedSubscription');
-    })->name('staff.sub.reject');
-
-    Route::get('testing', function () {
-        return view('test');
-    });
-    Route::get('create-ratecard/success', function () {
-        return view('notifications.register_success');
-    })->name('staff.ratecard.success');
-
-
-    //RATE CARDS
-    Route::get('rate-card-title', function () {
-        return view('staffDashboard.createRateCardTitle');
-    })->name('staff.create.ratecard');
-
-    //    Route::get('downloadable-subscriptions',function (){
-    //        return view('staffDashboard.downloadable-subs');
-    //    })->name('downloadable.subs');
-
-    Route::get('create-rate-card', function () {
-        return view('staffDashboard.createRateCard');
-    })->name('staff.create.rate.cards');
-
-
-    Route::get('view-rate-card', function () {
-        return view('staffDashboard.viewRateCard');
-    })->name('staff.view.ratecard');
-
-    //    Route::get('manage-admins',function (){
-    //        return view('staffDashboard.manageAdmin');
-    //    })->name('manage.admins');
-
-    Route::get('create-admins', function () {
-        return view('staffDashboard.createAdmin');
-    })->name('staff.create.admins');
-
-    Route::post('store-ratecard/api', 'RateCardController@storeRateCards');
-    Route::post('rate-card-title/api-store', 'RateCardController@storeRateCardTitle')->name('store.rate.card.title');
-    Route::get('ratecard-title/api/{ratecard_id}', 'RateCardController@fetchRateCardTitle');
-    Route::get('fetch-ratecards/api', 'RateCardController@getRateCards')->name('fetch.rate.cards');
-    Route::get('view-ratecard/api', 'RateCardController@viewRateCard')->name('view.rate.card');
-    Route::post('delete-ratecard/api', 'RateCardController@deleteRateCard')->name('delete.rate.card');
-
-    // RATE  CARDS ENDS HERE
-
-    Route::post('ads-store', 'SubController@createSub');
-    Route::get('ads/media-api', 'SubController@loadMedia');
-    Route::get('fetch-ads/api', 'SubController@fetchAds');
-    Route::get('media-houses-api/{media}', 'SubController@fetchMediaHouse');
-    Route::get('media-program-api/{mediaHouse}', 'SubController@fetchMediaProgram');
-    Route::get('program-date-api/{mediaProgram}', 'SubController@fetchProgramDates');
-    Route::get('fetch-segments-titles/{id}', 'SegmentController@fetchSegmentTitles');
-    Route::get('fetch-segments/{mediaHouse}/{segmentTitle}/{date}', 'SegmentController@fetchSegments');
-    Route::get('check-spots-api/{segment}', 'SegmentController@checkSpots');
-    Route::post('sub-update-api', 'SubController@updateAds');
-    Route::get('downloadable-subscriptions', 'SubController@downloadableSub')->name('staff.downloadable.subs');
-    Route::get('download-file/{id}', 'SubController@downloadSub')->name('download.sub');
-
-    //admin controller routes
-    Route::get('manage-admins', 'AdminController@viewAdmins')->name('staff.manage.admins');
-    Route::post('/store-admin', 'AdminController@storeAdmin')->name('staff.admin.store');
-    Route::get('admin-edit', 'AdminController@editAdmin')->name('staff.edit.admin');
-    Route::post('admin-update', 'AdminController@updateAdmin')->name('staff.update.admin');
-    Route::post('block-admin/api', 'AdminController@blockAdmin')->name('staff.block.admin');
-    Route::post('unblock-admin/api', 'AdminController@unblockAdmin')->name('staff.unblock.admin');
-    Route::post('admin-profile/update', 'AdminController@adminProfileUpdate')->name('staff.admin.profile');
-    Route::post('delete-admin/api', 'AdminController@deleteAdmin')->name('staff.delete.admin');
-
-    Route::post('/tests', 'AdminController@test')->name('test.store');
-
-    // media house admin routes
-    Route::get('fetch-pending-subs/api', 'SubController@fetchPendingSubs');
-    Route::post('update-subs-status/api', 'SubController@updateSubStatus');
-    Route::get('fetch-accepted-subs/api', 'SubController@fetchAcceptedSubs');
-    Route::get('fetch-active-subs/api', 'SubController@fetchActiveSubs');
-    Route::get('fetch-rejected-subs/api', 'SubController@fetchRejectedSubs');
-    Route::get('fetch-expired-subs/api', 'SubController@fetchExpiredSubs');
-    Route::get('fetch-all-subs/api', 'SubController@fetchAllSubs');
-//    Route::post('accept-sub/api', 'SubController@acceptSubs')->name('accept.subs');
-//    Route::post('reject-sub/api', 'SubController@rejectSubs')->name('reject.subs');
-
-    Route::get('test-date', 'SubController@testDate');
-    Route::post('test-date', 'SubController@demo')->name('demo');
-
-    //fetch total subscription for login in media house and display on dashboard
-    Route::get('fetch-total-ads/api', 'DashboardController@fetchTotalSubs');
-    Route::get('api/search', 'SearchController@searchSub');
-    Route::get('fetch-ratecards/api', 'RateCardController@fetchRateCards');
-
-    Route::get('test-api', 'SegmentController@api');
-    Route::post('test-post', 'SegmentController@apiPost');
-    Route::post('test-update-event', 'SegmentController@eventUpdate');
-    Route::post('test-check-segment', 'SegmentController@eventCheck');
-    Route::post('check-sub/api', 'SubController@checkIfSubExist');
-    Route::get('fetch-transac/api', 'SubController@fetchUserTransac');
-
-    // segment routs
-    Route::get('fetch-media-types-api', 'SegmentController@fetchMediaTypes');
-    Route::post('api-payment', 'PaymentController@payment');
-    Route::get('check-segment-api/{sub_date}', 'CheckSubController@checkSubAvailable');
-
-    //get fie for view routes
-    Route::get('fetch-file/api', 'SubController@getFile');
-});
-
-
-Route::post('/check-admin', 'Admin\AdminTypeController@checkAdminType')->name('check.admin');
-//Route::post('/login-staff', 'Admin\Auth\LoginStaffController@adminLogin')->name('login.staff');
-
-Route::get('/admin/login', 'Admin\Auth\LoginStaffController@showLoginForm')->name('staff.login');
-
-//Route::prefix('/media/staff')->group(function (){
-
-Route::get('/login', 'Admin\Auth\LoginStaffController@showAdminLoginForm')->name('login.form');
-Route::post('/login', 'Admin\Auth\LoginStaffController@adminLogin')->name('login.staff');
-Route::post('/logout', 'Admin\Auth\LoginStaffController@logout')->name('staff.logout');
+//Route::prefix('media/staff')->group(function () {
+//    Route::get('dashboard', function () {
+//        return view('staffDashboard.dashboard');
+//    })->name('staff.dashboard');
+//
+//    Route::get('active-subscription', function () {
+//        return view('staffDashboard.activeSubscription');
+//    })->name('staff.sub.active');
+//
+//
+//    Route::get('inactive-subscription', function () {
+//        return view('staffDashboard.inactiveSubscription');
+//    })->name('staff.sub.inactive');
+//
+//    Route::get('expired-subscription', function () {
+//        return view('staffDashboard.expiredSubscription');
+//    })->name('staff.sub.expired');
+//
+//
+//    Route::get('pending-subscription', function () {
+//        return view('staffDashboard.pendingSubscription');
+//    })->name('staff.sub.pending');
+//
+//    Route::get('transactions', function () {
+//        return view('staffDashboard.transactions');
+//    })->name('staff.transact');
+//
+//    Route::get('user-profile', function () {
+//        return view('staffDashboard.accountSettings');
+//    })->name('staff.profile');
+//
+//    Route::get('new-subscriptions', function () {
+//        return view('staffDashboard.newSubscription');
+//    })->name('staff.sub.new');
+//
+//    Route::get('dashboard', function () {
+//        $media = \App\User::all('logo');
+//        return view('staffDashboard.dashboard', ['media' => $media]);
+//    })->name('staff.dashboard');
+//
+//    Route::get('subscription', function () {
+//        return view('staffDashboard.createSub');
+//    })->name('staff.sub.all');
+//
+//    Route::get('rejected-subscription', function () {
+//        return view('staffDashboard.rejectedSubscription');
+//    })->name('staff.sub.reject');
+//
+//    Route::get('testing', function () {
+//        return view('test');
+//    });
+//    Route::get('create-ratecard/success', function () {
+//        return view('notifications.register_success');
+//    })->name('staff.ratecard.success');
+//
+//
+//    //RATE CARDS
+//    Route::get('rate-card-title', function () {
+//        return view('staffDashboard.createRateCardTitle');
+//    })->name('staff.create.ratecard');
+//
+//    //    Route::get('downloadable-subscriptions',function (){
+//    //        return view('staffDashboard.downloadable-subs');
+//    //    })->name('downloadable.subs');
+//
+//    Route::get('create-rate-card', function () {
+//        return view('staffDashboard.createRateCard');
+//    })->name('staff.create.rate.cards');
+//
+//
+//    Route::get('view-rate-card', function () {
+//        return view('staffDashboard.viewRateCard');
+//    })->name('staff.view.ratecard');
+//
+//    //    Route::get('manage-admins',function (){
+//    //        return view('staffDashboard.manageAdmin');
+//    //    })->name('manage.admins');
+//
+//    Route::get('create-admins', function () {
+//        return view('staffDashboard.createAdmin');
+//    })->name('staff.create.admins');
+//
+//    Route::post('store-ratecard/api', 'RateCardController@storeRateCards');
+//    Route::post('rate-card-title/api-store', 'RateCardController@storeRateCardTitle')->name('store.rate.card.title');
+//    Route::get('ratecard-title/api/{ratecard_id}', 'RateCardController@fetchRateCardTitle');
+//    Route::get('fetch-ratecards/api', 'RateCardController@getRateCards')->name('fetch.rate.cards');
+//    Route::get('view-ratecard/api', 'RateCardController@viewRateCard')->name('view.rate.card');
+//    Route::post('delete-ratecard/api', 'RateCardController@deleteRateCard')->name('delete.rate.card');
+//
+//    // RATE  CARDS ENDS HERE
+//
+//    Route::post('ads-store', 'SubController@createSub');
+//    Route::get('ads/media-api', 'SubController@loadMedia');
+//    Route::get('fetch-ads/api', 'SubController@fetchAds');
+//    Route::get('media-houses-api/{media}', 'SubController@fetchMediaHouse');
+//    Route::get('media-program-api/{mediaHouse}', 'SubController@fetchMediaProgram');
+//    Route::get('program-date-api/{mediaProgram}', 'SubController@fetchProgramDates');
+//    Route::get('fetch-segments-titles/{id}', 'SegmentController@fetchSegmentTitles');
+//    Route::get('fetch-segments/{mediaHouse}/{segmentTitle}/{date}', 'SegmentController@fetchSegments');
+//    Route::get('check-spots-api/{segment}', 'SegmentController@checkSpots');
+//    Route::post('sub-update-api', 'SubController@updateAds');
+//    Route::get('downloadable-subscriptions', 'SubController@downloadableSub')->name('staff.downloadable.subs');
+//    Route::get('download-file/{id}', 'SubController@downloadSub')->name('download.sub');
+//
+//    //admin controller routes
+//    Route::get('manage-admins', 'AdminController@viewAdmins')->name('staff.manage.admins');
+//    Route::post('/store-admin', 'AdminController@storeAdmin')->name('staff.admin.store');
+//    Route::get('admin-edit', 'AdminController@editAdmin')->name('staff.edit.admin');
+//    Route::post('admin-update', 'AdminController@updateAdmin')->name('staff.update.admin');
+//    Route::post('block-admin/api', 'AdminController@blockAdmin')->name('staff.block.admin');
+//    Route::post('unblock-admin/api', 'AdminController@unblockAdmin')->name('staff.unblock.admin');
+//    Route::post('admin-profile/update', 'AdminController@adminProfileUpdate')->name('staff.admin.profile');
+//    Route::post('delete-admin/api', 'AdminController@deleteAdmin')->name('staff.delete.admin');
+//
+//    Route::post('/tests', 'AdminController@test')->name('test.store');
+//
+//    // media house admin routes
+//    Route::get('fetch-pending-subs/api', 'SubController@fetchPendingSubs');
+//    Route::post('update-subs-status/api', 'SubController@updateSubStatus');
+//    Route::get('fetch-accepted-subs/api', 'SubController@fetchAcceptedSubs');
+//    Route::get('fetch-active-subs/api', 'SubController@fetchActiveSubs');
+//    Route::get('fetch-rejected-subs/api', 'SubController@fetchRejectedSubs');
+//    Route::get('fetch-expired-subs/api', 'SubController@fetchExpiredSubs');
+//    Route::get('fetch-all-subs/api', 'SubController@fetchAllSubs');
+////    Route::post('accept-sub/api', 'SubController@acceptSubs')->name('accept.subs');
+////    Route::post('reject-sub/api', 'SubController@rejectSubs')->name('reject.subs');
+//
+//    Route::get('test-date', 'SubController@testDate');
+//    Route::post('test-date', 'SubController@demo')->name('demo');
+//
+//    //fetch total subscription for login in media house and display on dashboard
+//    Route::get('fetch-total-ads/api', 'DashboardController@fetchTotalSubs');
+//    Route::get('api/search', 'SearchController@searchSub');
+//    Route::get('fetch-ratecards/api', 'RateCardController@fetchRateCards');
+//
+//    Route::get('test-api', 'SegmentController@api');
+//    Route::post('test-post', 'SegmentController@apiPost');
+//    Route::post('test-update-event', 'SegmentController@eventUpdate');
+//    Route::post('test-check-segment', 'SegmentController@eventCheck');
+//    Route::post('check-sub/api', 'SubController@checkIfSubExist');
+//    Route::get('fetch-transac/api', 'SubController@fetchUserTransac');
+//
+//    // segment routs
+//    Route::get('fetch-media-types-api', 'SegmentController@fetchMediaTypes');
+//    Route::post('api-payment', 'PaymentController@payment');
+//    Route::get('check-segment-api/{sub_date}', 'CheckSubController@checkSubAvailable');
+//
+//    //get fie for view routes
+//    Route::get('fetch-file/api', 'SubController@getFile');
 //});
-
-Route::prefix('/media/staff')->group(function () {
-    Route::get('/login', 'staffAuth\LoginController@showLoginForm')->name('login.staff');
-    Route::post('/login', 'staffAuth\LoginController@login')->name('login.staff');
-    Route::post('/logout', 'staffAuth\LoginController@logout')->name('logout.staff');
-
-    // Route::post('/logout', 'staffAuth\LoginStaffController@logout')->name('staff.logout');
-
-    Route::get('week-data', 'SubController@weekData');
-});
-
-
-
-Route::prefix('auth')->group(function () {
-    Auth::routes();
-});
+//
+//
+//Route::post('/check-admin', 'Admin\AdminTypeController@checkAdminType')->name('check.admin');
+////Route::post('/login-staff', 'Admin\Auth\LoginStaffController@adminLogin')->name('login.staff');
+//
+//Route::get('/admin/login', 'Admin\Auth\LoginStaffController@showLoginForm')->name('staff.login');
+//
+////Route::prefix('/media/staff')->group(function (){
+//
+//Route::get('/login', 'Admin\Auth\LoginStaffController@showAdminLoginForm')->name('login.form');
+//Route::post('/login', 'Admin\Auth\LoginStaffController@adminLogin')->name('login.staff');
+//Route::post('/logout', 'Admin\Auth\LoginStaffController@logout')->name('staff.logout');
+////});
+//
+//Route::prefix('/media/staff')->group(function () {
+//    Route::get('/login', 'staffAuth\LoginController@showLoginForm')->name('login.staff');
+//    Route::post('/login', 'staffAuth\LoginController@login')->name('login.staff');
+//    Route::post('/logout', 'staffAuth\LoginController@logout')->name('logout.staff');
+//
+//    // Route::post('/logout', 'staffAuth\LoginStaffController@logout')->name('staff.logout');
+//
+//    Route::get('week-data', 'SubController@weekData');
+//});
+//
+//
+//
+//Route::prefix('auth')->group(function () {
+//    Auth::routes();
+//});
 
 
 
