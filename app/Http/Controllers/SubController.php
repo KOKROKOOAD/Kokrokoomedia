f2b2d87a0c313a9ce0c1f8b5b7e37cbed5bbbe61
Joojo Arthur <jarthur@nalosolutions.com>
2020-01-20 00:59:53 +0000

    fix  view and approve sub logic

--- app/Http/Controllers/SubController.php
+++ app/Http/Controllers/SubController.php
@@ -199,7 +199,7 @@ class SubController extends Controller
 
     public function viewFile($id){
         $sub = ScheduledAd::all('file_name','file_path','file_size')->where('subscription_id',$id);
-        dd($sub);
+        dd($id);
         return view('userDashboard.viewSubDetails')->with('sub',$sub);
     }
 
